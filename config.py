c82d0344fd0ba43f26a57cb965b7d254886295e4
N3TH1K4 <88619598+N3TH1K4@users.noreply.github.com>
2022-10-08 18:06:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,3 @@
 from os import getenv
 from dotenv import load_dotenv
-
-load_dotenv()
-
-API_ID = 18983092
-API_HASH = "a6005a70e88369b4fb08b8350ebbdd35"
-BOT_TOKEN = "5346281142:AAGyQRPmb-teZrkI8AaJnSRDWxVdfJbrvuc"
-
-MONGO_DB_URI =  "mongodb+srv://Manudark:dark@cluster0.qkscpno.mongodb.net/?retryWrites=true&w=majority"
-INDEX_ID = -1001672379327
-UPLOADS_ID = -1001740801934
-
-STATUS_ID = 6506
-SCHEDULE_ID = 6507
-
-CHANNEL_TITLE = "Airing Anime [ON]"
-INDEX_USERNAME = "Otaku_Network"
-UPLOADS_USERNAME = "Airing_Anime_ON"
+s
